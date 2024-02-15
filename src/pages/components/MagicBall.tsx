d431ac219482d841565a0dea42521bdcc935d397
RachelX <113003027+raxron@users.noreply.github.com>
2024-02-15 13:07:50 -0800

    spline added;

---
+++ src/pages/components/MagicBall.tsx
@@ -0,0 +1,10 @@
+import Spline from '@splinetool/react-spline';
+
+export default function MagicBall() {
+  return (
+    <div className='absolute'>
+         <Spline scene="https://prod.spline.design/79B0-GlGN4GFFp3g/scene.splinecode" />
+    </div>
+   
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,29 @@
 import Image from "next/image";
 import Nav from "./components/MyNav";
+import MagicBall from "./components/magicBall";
 export default function Home() {
   return (
     <main
       className="flex min-h-screen flex-col items-center justify-between p-24 bg-beige text-black font-josefine"
     >
       <Nav />
-      <div className="flex w-full justify-between">
-        <div>
-          <h2>Let the Stars Guide You</h2>
-        </div>
+      <div className="flex w-full justify-center flex-col items-center">
+
         <Image src="/assets/blackLogo.png" width={1000} height={1000} alt="logo black" className="w-80 h-auto" />
-        <div>
-          <p>being one of us</p>
-        </div>
+        <MagicBall />
       </div>
 
-      <div className="w-full h-32 bg-black text-beige rounded-3xl flex items-center justify-center">
-        Let's Find Your Besties!
+      <div className="w-full h-24 bg-black text-beige rounded-3xl flex items-center justify-center gap-2">
+        <p>Let the Starts Guide You</p>
+        <div className="w-4 h-4"
+          style={{
+            "borderLeft": "12px solid transparent",
+            "borderRight": "12px solid transparent",
+            "borderTop": "12px solid beige",
+            "borderRadius": "10px",
+          }}>
+          <span></span>
+        </div>
       </div>
     </main>
   );
